67ad75ddd0093d9853ad123ce8add0ffe4f435ee
unknown <arnauldkayonga1@gmail.com>
2021-11-17 22:42:50 +0200

    Update 1.1

--- System/urls.py
+++ System/urls.py
@@ -1,4 +1,4 @@
-"""EwaweDashboard URL Configuration
+"""System URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
