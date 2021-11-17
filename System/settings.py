b0f8bd6918bf3f4b6156ea553467fe6c6274cf7d
unknown <arnauldkayonga1@gmail.com>
2021-11-17 23:08:13 +0200

    Fix static files server

--- System/settings.py
+++ System/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Static'),]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
