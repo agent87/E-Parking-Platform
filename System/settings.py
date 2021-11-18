ce2c1c67d837d26eb88ad34c26630482707f0254
unknown <arnauldkayonga1@gmail.com>
2021-11-18 18:31:51 +0200

    Update Static source

--- System/settings.py
+++ System/settings.py
@@ -132,9 +132,12 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Static'),]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 # Default primary key field type
