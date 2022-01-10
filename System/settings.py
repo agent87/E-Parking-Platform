407ceeb0d7e9029d1dd1f4887862c7f89b5f9003
unknown <arnauldkayonga1@gmail.com>
2022-01-10 14:53:42 +0200

    Fixed email server settings env secrets key names

--- System/settings.py
+++ System/settings.py
@@ -155,13 +155,7 @@ environ.Env.read_env()
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_HOST = os.environ.get('SMTP_HOST')
+EMAIL_HOST_USER = os.environ.get('SMTP_SERVER_USR')
+EMAIL_HOST_PASSWORD = os.environ.get('SMTP_SERVER_PSWRD')
 
-try: 
-    EMAIL_HOST = os.environ.get('SMTP_HOST')
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USR')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PSWRD')
-
-except ImproperlyConfigured:
-    EMAIL_HOST = env('EMAIL_HOST')
-    EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
